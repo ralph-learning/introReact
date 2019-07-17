ff32e49a5b4a6e76ba8d184b0cf437d3a4db65d1
Ralph Effting <ralph@brainn.co>
2019-07-17 20:14:52 -0300

    Add useEffect for fetch

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,12 +1,22 @@
-import React, { useState } from 'react';
-import { ANIMALS } from '@frontendmasters/pet';
+import React, { useState, useEffect } from 'react';
+import pet, { ANIMALS } from '@frontendmasters/pet';
 import useDropdown from './useDropdown';
 
 const SearchParams = () => {
-  const [breeds, setBrreds] = useState([]);
+  const [breeds, setBreeds] = useState([]);
   const [location, setLocation] = useState('Porto Alegre');
-  const [animal, DropDownAnimal, setAnimal] = useDropdown('Animal', 'Dog', ANIMALS);
+  const [animal, DropDownAnimal] = useDropdown('Animal', 'Dog', ANIMALS);
   const [breed, DropDownBreed, setBreed] = useDropdown('Breed', '', breeds);
+
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedString = breeds.map(({ name }) => name);
+      setBreeds(breedString);
+    }, console.error);
+  }, [animal, setBreed]);
 
   return (
     <div className="search-params">
