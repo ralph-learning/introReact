780f3fa2d3c871d2f8f06090d5dcddcc6289352d
Ralph Effting <ralph@brainn.co>
2019-07-18 09:32:00 -0300

    Create a detail page amd request info based on id

--- src/Details.js
+++ src/Details.js
@@ -1,8 +1,47 @@
 import React from 'react';
+import pet from '@frontendmasters/pet';
 
-const Details = () => (
-  <div>Hello world</div>
-);
+class Details extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    pet.animal(this.props.id)
+      .then(({animal}) => {
+        this.setState({
+          name: animal.name,
+          animal: animal.type,
+          location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+          description: animal.description,
+          media: animal.photos,
+          breed: animal.breeds.primary,
+          loading: false,
+        });
+      }, console.error);
+  }
+
+  render() {
+    const { animal, breed, location, description, loading } = this.state;
+
+    if(loading) return <h1>Loading...</h1>;
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal}, ${breed}, ${location}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Details;
 
