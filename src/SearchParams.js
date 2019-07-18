9c7e40a255ffc3caf506513d0a3da96d618451de
Ralph Effting <ralfting@gmail.com>
2019-07-17 23:10:17 -0300

    Create pets state Get pets data using requestPet with async/await

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,12 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import pet, { ANIMALS } from '@frontendmasters/pet';
 import useDropdown from './useDropdown';
+import Results from './Results';
 
 const SearchParams = () => {
   const [breeds, setBreeds] = useState([]);
-  const [location, setLocation] = useState('Porto Alegre');
+  const [location, setLocation] = useState('Seattle, WA');
+  const [pets, setPets] = useState([]);
+
   const [animal, DropDownAnimal] = useDropdown('Animal', 'Dog', ANIMALS);
   const [breed, DropDownBreed, setBreed] = useDropdown('Breed', '', breeds);
+
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal
+    });
+
+    setPets(animals)
+  }
 
   useEffect(() => {
     setBreeds([]);
@@ -20,7 +33,12 @@ const SearchParams = () => {
 
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          requestPets()}
+        }
+      >
         <label htmlFor="location">
           <input
             id="location"
@@ -36,6 +54,7 @@ const SearchParams = () => {
 
         <button>Submit</button>
       </form>
+      <Results pets={pets} />
     </div>
   );
 };
