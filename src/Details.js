651653f5e4dd9089914eba0ede65ce417bd58361
Ralph Effting <ralph@brainn.co>
2019-07-20 10:47:57 -0300

    Create carousel component

--- src/Details.js
+++ src/Details.js
@@ -1,41 +1,60 @@
-import React from 'react';
-import pet from '@frontendmasters/pet';
+import React from "react";
+import pet from "@frontendmasters/pet";
+import { navigate } from "@reach/router";
+import Carousel from "./Carousel";
 
-class Details extends React.Component {
-  state = { loading: true, name: "" }
-
+export default class Details extends React.Component {
+  state = { loading: true, showModal: false };
   componentDidMount() {
-    pet.animal(this.props.id)
-      .then(({animal}) => {
+    pet
+      .animal(this.props.id)
+      .then(({ animal }) => {
         this.setState({
           name: animal.name,
           animal: animal.type,
-          location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+          location: `${animal.contact.address.city}, ${
+            animal.contact.address.state
+          }`,
           description: animal.description,
           media: animal.photos,
           breed: animal.breeds.primary,
-          loading: false,
+          url: animal.url,
+          loading: false
         });
-      }, console.error);
+      })
+      .catch(err => this.setState({ error: err }));
   }
-
+  toggleModal = () => this.setState({ showModal: !this.state.showModal });
+  adopt = () => navigate(this.state.url);
   render() {
-    const { animal, breed, location, description, loading } = this.state;
+    if (this.state.loading) {
+      return <h1>loading … </h1>;
+    }
 
-    if(loading) return <h1>Loading...</h1>;
+    const {
+      animal,
+      breed,
+      location,
+      description,
+      media,
+      name,
+    } = this.state;
 
     return (
       <div className="details">
+        <Carousel media={media} />
         <div>
           <h1>{name}</h1>
-          <h2>{`${animal}, ${breed}, ${location}`}</h2>
-          <button>Adopt {name}</button>
+          <h2>{`${animal} — ${breed} — ${location}`}</h2>
+          <button
+            onClick={this.toggleModal}
+          >
+            Adopt {name}
+          </button>
           <p>{description}</p>
         </div>
       </div>
     );
   }
 }
-
-export default Details;
 
